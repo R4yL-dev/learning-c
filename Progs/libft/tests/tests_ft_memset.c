c809463d2d4b876a4ee3fdf337b0ca734220714c
Ray-cmd <luca.ray@protonmail.ch>
2022-09-28 03:34:54 +0200

    add memset

---
+++ Progs/libft/tests/tests_ft_memset.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tests_ft_memset.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: luca <luca@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/28 02:51:25 by luca              #+#    #+#             */
+/*   Updated: 2022/09/28 03:34:20 by luca             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
+
+void	*ft_memset(void *s, int c, size_t n);
+
+int	main(void)
+{
+	char	*ptr1;
+	char	*ptr2;
+	char	str[20];
+	size_t	i;
+
+	ptr1 = ft_memset(str, 120, 11);
+	ptr2 = memset(str, 120, 11);
+	i = 0;
+	while (i <= 20)
+	{
+		printf("ptr1[%ld] = %d | ptr2[%ld] = %d\n", i, ptr1[i], i, ptr2[i]);
+		i++;
+	}
+	return (0);
+}

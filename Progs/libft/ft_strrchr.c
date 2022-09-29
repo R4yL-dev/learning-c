31e150e3e4390f0f315aed49d36ed31dc3a74f44
Ray-cmd <luca.ray@protonmail.ch>
2022-09-29 17:42:12 +0200

    add ft_strrchr.c and tests

---
+++ Progs/libft/ft_strrchr.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: luca <luca@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/29 17:27:02 by luca              #+#    #+#             */
+/*   Updated: 2022/09/29 17:27:56 by luca             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strrchr(const char *s, int c)
+{
+	char *str;
+	char *resp;
+	int size;
+	int i;
+
+	str = (char *)s;
+	resp = NULL;
+	size = ft_strlen(s);
+	i = 0;
+	while (i <= (size))
+	{
+		if (s[i] == c)
+			resp = &str[i];
+		i++;
+	}
+	return (resp);
+}
---
+++ Progs/libft/tests/tests_ft_strrchr.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tests_ft_strrchr.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: luca <luca@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/29 17:28:24 by luca              #+#    #+#             */
+/*   Updated: 2022/09/29 17:31:09 by luca             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../libft.h"
+#include <string.h>
+#include <stdio.h>
+
+int	main(void)
+{
+	char	*s1 = "salut les amis";
+	char	*resp1;
+	char	*resp2;
+	int		i;
+	int		err;
+
+	err = 0;
+	i = -10;
+	while (i <= 250)
+	{
+		resp1 = ft_strrchr(s1, i);
+		resp2 = strrchr(s1, i);
+		printf("addr with ft_strchr() = %p | addr with strchr() = %p | char = %c\n"\
+		, resp1, resp2, i);
+		if (resp1 != resp2)
+		{
+			printf("[Err] result of ft_strchr don't match with result of strchr\n");
+			printf("[Inf] #%d %p != %p\n | char %c\n", i, resp1, resp2, i);
+			err++;
+		}
+		i++;
+	}
+	printf("[Inf] test end with %d error(s)\n", err);
+	return (0);
+}

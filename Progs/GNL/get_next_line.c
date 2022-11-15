5fad4350a7fdbad622c3f1f94161ee4a94283ea3
Ray-cmd <luca.ray@protonmail.ch>
2022-11-15 16:28:41 +0100

    Creation of GNL

---
+++ Progs/GNL/get_next_line.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lray <lray@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/15 16:24:24 by lray              #+#    #+#             */
+/*   Updated: 2022/11/15 16:27:31 by lray             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+	Desc :
+		Write a fucntion that returns a line read from a file descriptor.
+
+	Params :
+		- fd : The file descriptor to read from.
+
+	Ret :
+		Read line if correct behavior.
+		NULL of there is nothing else to read, or an error occurred.
+*/
+
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+
+}

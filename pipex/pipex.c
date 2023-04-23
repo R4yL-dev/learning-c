cdca5e1483fc29069f583dd76805f10f5ff0bcac
Luca <luca.ray@protonmail.ch>
2023-04-23 20:50:37 +0200

    exec cmd from PATH and clean error

--- pipex/pipex.c
+++ pipex/pipex.c
@@ -6,13 +6,12 @@
 /*   By: lray <lray@student.42lausanne.ch >         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/22 18:12:56 by lray              #+#    #+#             */
-/*   Updated: 2023/04/22 21:31:49 by lray             ###   ########.fr       */
+/*   Updated: 2023/04/23 20:44:55 by lray             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-static void	exit_error(char *msg);
 static void	child_process(char *cmd, char **envp, int in_fd, int out_fd);
 
 int	main(int argc, char **argv, char **envp)
@@ -24,24 +23,24 @@ int	main(int argc, char **argv, char **envp)
 	pid_t	pid2;
 
 	if (argc != 5)
-		exit_error("Invalid number of arguments");
+		exit_error(errno, "Invalid number of arguments");
 	file1 = open(argv[FILE1_POS], O_RDONLY);
 	if (file1 == -1)
-		exit_error("Failed to open file 1");
+		exit_error(errno, "Failed to open file 1");
 	file2 = open(argv[FILE2_POS], O_CREAT | O_WRONLY | O_TRUNC, 0644);
 	if (file2 == -1)
-		exit_error("Failed to open file 2");
+		exit_error(errno, "Failed to open file 2");
 	if (pipe(pipes) == -1)
-		exit_error("Failed to create pipe");
+		exit_error(errno, "Failed to create pipe");
 	pid1 = fork();
 	if (pid1 == -1)
-		exit_error("Failed to fork");
+		exit_error(errno, "Failed to fork");
 	else if (pid1 == 0)
 		child_process(argv[CMD1_POS], envp, file1, pipes[1]);
 	close(pipes[1]);
 	pid2 = fork();
 	if (pid2 == -1)
-		exit_error("Failed to fork");
+		exit_error(errno, "Failed to fork");
 	else if (pid2 == 0)
 		child_process(argv[CMD2_POS], envp, pipes[0], file2);
 	close(pipes[0]);
@@ -52,18 +51,6 @@ int	main(int argc, char **argv, char **envp)
 	return (EXIT_SUCCESS);
 }
 
-/*
-	TODO: Mettre cette fonction dans pipex_utils.c
-*/
-static void	exit_error(char *msg)
-{
-	perror(msg);
-	exit(EXIT_FAILURE);
-}
-
-/*
-	TODO: Gerer le PATH plutot que de juster aller check /bin/
-*/
 static void	child_process(char *cmd, char **envp, int in_fd, int out_fd)
 {
 	char	**cmd_args;
@@ -71,16 +58,27 @@ static void	child_process(char *cmd, char **envp, int in_fd, int out_fd)
 
 	cmd_args = ft_split(cmd, ' ');
 	if (!cmd_args)
-		exit_error("Failed to split command");
-	path = ft_strjoin("/bin/", cmd_args[0]);
+		exit_error(errno, "Failed to split command");
+	path = has_cmd(cmd_args[0], envp);
 	if (!path)
-		exit_error("Failed to join command path");
+	{
+		free_array(cmd_args);
+		exit_error(errno, "Command not found");
+	}
 	if (dup2(in_fd, STDIN_FILENO) == -1)
-		exit_error("Failed to dup2");
+	{
+		free_array(cmd_args);
+		free(path);
+		exit_error(errno, "Failed to dup2");
+	}
 	if (dup2(out_fd, STDOUT_FILENO) == -1)
-		exit_error("Failed to dup2");
+	{
+		free_array(cmd_args);
+		free(path);
+		exit_error(errno, "Failed to dup2");
+	}
 	close(in_fd);
 	close(out_fd);
 	execve(path, cmd_args, envp);
-	exit_error("Failed to execute command");
+	exit_error(errno, "Failed to execute command");
 }

e267a6840ce9e93034b22b6dc63da3578e719763
Luca Ray <lray@student.42lausanne.ch>
2023-04-26 16:56:16 +0200

    optimize version of sort5

--- push_swap/README.md
+++ push_swap/README.md
@@ -9,8 +9,8 @@
 - [x] Add ft_printf and ft_stack to libft (should validate ft_printf to do so).
 - [x] Compile libft from source when compiling project.
 - [x] Benchmark to be sure to pass the validation.
-  - stack size : 3 - Average 1
-  - stack size : 5 - Average 8
-  - stack size : 100 - Average 1084
-  - stack size : 500 - Average 6764
+  - stack size : 3 - Average 1 / Min 0 / Max 2
+  - stack size : 5 - Average 8 / Min 0 / Max 10
+  - stack size : 100 - Average 1084 / Min 1084 / Max 1084
+  - stack size : 500 - Average 6764 / Min 6784 / Max 6784
 - [x] Check leaks
--- push_swap/sort5.c
+++ push_swap/sort5.c
@@ -6,22 +6,30 @@
 /*   By: lray <lray@student.42lausanne.ch >         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 17:51:58 by lray              #+#    #+#             */
-/*   Updated: 2023/03/20 11:39:25 by lray             ###   ########.fr       */
+/*   Updated: 2023/04/25 15:07:30 by lray             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+/*
+	TODO: Once evaluate, put exec_rb into exec_move.c
+*/
 void	sort5(t_stack **stack_a, t_stack **stack_b)
 {
 	if (!is_sorted(stack_a))
 	{
-		while ((*stack_a)->data != 0)
-			exec_rra(stack_a);
+		while ((*stack_a)->data != 0 && (*stack_a)->data != 1)
+			exec_ra(stack_a);
 		exec_pb(stack_a, stack_b);
-		while ((*stack_a)->data != 1)
-			exec_rra(stack_a);
+		while ((*stack_a)->data != 0 && (*stack_a)->data != 1)
+			exec_ra(stack_a);
 		exec_pb(stack_a, stack_b);
+		if ((*stack_b)->data == 0)
+		{
+			ft_printf("rb\n");
+			mv_rotate(stack_b);
+		}
 		sort3(stack_a);
 		exec_pa(stack_a, stack_b);
 		exec_pa(stack_a, stack_b);

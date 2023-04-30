ca00172780672b98fe3f3eac989d894ef6803b55
Luca <luca.ray@protonmail.ch>
2023-04-30 18:44:40 +0200

    adapte to new strucutre

--- so_long/main.c
+++ so_long/main.c
@@ -5,110 +5,46 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: lray <lray@student.42lausanne.ch >         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/11 16:17:40 by lray              #+#    #+#             */
-/*   Updated: 2023/04/14 14:55:41 by lray             ###   ########.fr       */
+/*   Created: 2023/04/29 17:32:37 by lray              #+#    #+#             */
+/*   Updated: 2023/04/30 18:25:33 by lray             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
-#include <stdio.h>
-#include <unistd.h>
 
-int	win_close(void);
+static int	get_cel(t_map **map, unsigned int x, unsigned int y);
 
-int	key_listener(int kc)
+int	main(int argc, char **argv)
 {
-	printf("keycode = %d\n", kc);
-	if (kc == 53)
-		win_close();
-	return (0);
-}
+	t_map	*map;
 
-int mk_listener(int mc)
-{
-	printf("mousecode = %d\n", mc);
-	return (0);
-}
-
-int mm_listener(int x, int y)
-{
-	printf("MX = %d MY = %d\n", x, y);
-	return (0);
-}
-
-int	win_close(void)
-{
-	exit(0);
-	return (0);
-}
-
-void	build_ground(void *ground_ptr)
-{
-	int	width = 800;
-	int	heigth = 600;
-	int	bpp = 32;
-	int	size_line;
-	char	*ground_data;
-	int		color = 0xFFFFFF;
-	int		offset;
-	int		x;
-	int		y;
-
-	ground_data = mlx_get_data_addr(ground_ptr, &bpp, &size_line, NULL);
-	x = 0;
-	y = 0;
-	while (x < width)
+	map = NULL;
+	if (!parse_args(&argc, &argv))
+		exit (EXIT_FAILURE);
+	if (!map_init(&map, argv[0]))
 	{
-		while (y < heigth)
-		{
-			offset = y * size_line + x * (bpp / 8);
-			*(unsigned int *)(ground_data + offset) = color;
-			//mlx_pixel_put(mlx, win, x, y, 0xFFFFFF);
-			y++;
-		}
-		y = 0;
-		x++;
+		map_free(&map);
+		exit (EXIT_FAILURE);
 	}
+	dbmap_show_format(&map);
+	dbmap_show_infos(&map);
+	get_cel(&map, 18, 8);
+	map_free(&map);
+	return (EXIT_SUCCESS);
 }
 
-int	main(void)
+static int	get_cel(t_map **map, unsigned int x, unsigned int y)
 {
-	void	*mlx;
-	void	*win;
-	void	*img_player;
-	int		img_h = 64;
-	int		img_w = 55;
-	//void	*ground_ptr;
-	//char	*ground_data;
-	int		i;
-	//int		bpp = 32;
-	//int		size_line;
-
-	mlx = mlx_init();
-	win = mlx_new_window(mlx, 800, 600, "Salut les connards");
-	mlx_hook(win, 17, 0, win_close, mlx);
-	mlx_hook(win, 2, 0, key_listener, mlx);
-	mlx_hook(win, 4, 0, mk_listener, mlx);
-	mlx_hook(win, 6, 0, mm_listener, mlx);
-
-	//ground_ptr = mlx_new_image(mlx, 800, 600);
-	//build_ground(ground_ptr);
-	//ground_data = mlx_get_data_addr(ground_ptr, &bpp, &size_line, NULL);
-	img_player = mlx_xpm_file_to_image(mlx, "img/player.xpm", &img_h, &img_w);
-	//img_data = mlx_get_data_addr(img_player, &bpp, &size_line, NULL);
-	i = 0;
-	while (1)
+	if (x >= (*map)->nbrs_cols)
 	{
-
-		mlx_clear_window(mlx, win);
-		//mlx_put_image_to_window(mlx, win, ground_ptr, 0, 0);
-		mlx_put_image_to_window(mlx, win, img_player, i, 0);
-		//usleep(50);
-		if (i )
-		i += 10;
-		mlx_do_sync(mlx);
+		put_error("x must not be greater than the number of colonnes");
+		return (0);
 	}
-	//mlx_loop(mlx);
-	mlx_destroy_window(mlx, win);
+	if (y >= (*map)->nbrs_rows)
+	{
+		put_error("y must not be greater than the number of rows");
+		return (0);
+	}
+	ft_printf("Char x:%d y:%d -> %c\n", x, y, (*map)->content[x + (y * ((*map)->nbrs_cols))]);
 	return (1);
 }
--- so_long/so_long.h
+++ so_long/so_long.h
@@ -5,13 +5,37 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: lray <lray@student.42lausanne.ch >         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/11 16:15:34 by lray              #+#    #+#             */
-/*   Updated: 2023/04/11 16:47:16 by lray             ###   ########.fr       */
+/*   Created: 2023/04/29 17:31:38 by lray              #+#    #+#             */
+/*   Updated: 2023/04/30 18:11:34 by lray             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
-# include <mlx.h>
-# include <stdlib.h>
+
+# include "libft/libft.h"
+
+typedef struct s_map
+{
+	char			*content;
+	unsigned int	nbrs_rows;
+	unsigned int	nbrs_cols;
+}	t_map;
+
+/*Parse arguments.*/
+int		parse_args(int *argc, char ***argv);
+
+/*Initialize map with file in path.*/
+int		map_init(t_map **map, char *path);
+/*Clean map*/
+void	map_free(t_map **map);
+
+/*Show error message on stderr.*/
+void	put_error(char *msg);
+
+/*Show the formatted map.*/
+void	dbmap_show_format(t_map **map);
+/*Show all infos about the map.*/
+void	dbmap_show_infos(t_map **map);
+
 #endif
